3931af3951cf6011142fecc63c61f22e3d0c842e
Jackline Sarah Zawedde <jackline.sarah.z@gmail.com>
2014-10-14 16:35:52 +0200

    removed unused code

--- actions/login.js
+++ actions/login.js
@@ -2,17 +2,4 @@
 
 (function () {
 	var api = require('../api/login.js');
-
-	exports.create = function(req, res) {
-		var username = req.body.customerId
-			, password = req.body.password;
-
-		if(api.login(username, password)) {
-			res.send('hello');
-		}
-		else {
-			res.statusCode = 401;
-			res.send("Unauthorized!");
-		}
-	};
 })();
